b81084f5aad2aaceb8a4fc368b0c07edc09b948d
Maya Luna Celeste <valkyriealts@gmail.com>
2020-08-20 19:31:07 -0400

    Initial Commit

---
+++ CubeNet/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  CubeNet
+//
+//  Created by Maya Luna Celeste on 8/20/20.
+//  Copyright © 2020 Maya Luna Celeste. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CubeNetTests/CubeNetTests.swift
@@ -0,0 +1,34 @@
+//
+//  CubeNetTests.swift
+//  CubeNetTests
+//
+//  Created by Maya Luna Celeste on 8/20/20.
+//  Copyright © 2020 Maya Luna Celeste. All rights reserved.
+//
+
+import XCTest
+@testable import CubeNet
+
+class CubeNetTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
